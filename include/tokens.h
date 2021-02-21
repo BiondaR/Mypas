5e9e44768916a0861759ef609341944bee46bc26
Bionda Rozin <51840763+BiondaR@users.noreply.github.com>
2021-02-21 16:32:35 -0300

    Update tokens.h

--- include/tokens.h
+++ include/tokens.h
@@ -1,8 +1,27 @@
 /**@<tokens.h>::**/
 
-//#define ID 1024
-//#define UINT 1025
+/*
+ * Bacharelado em Ciências da Computação
+ * UNESP Rio Claro
+ * Período Integral
+ * 
+ */
 
+ /* 
+ * Desenvolvido pelo grupo 3:
+ * Bionda Rozin
+ * Matheus Missio Francisco
+ * Nicholas Seiti Handa
+ * Nikolas Gomes de Sá
+ * 
+ */
+  
+ /*
+  * Data de criação: 21/02/2021
+  *
+  */
+
+/* Definição de constantes de tokens */
 enum {
 	ID = 1024,
 	UINT,
