d46e059186bb9da27d4048696cbaba53d1a063e7
Bionda Rozin <51840763+BiondaR@users.noreply.github.com>
2021-02-21 16:40:02 -0300

    Update parser.h

--- include/parser.h
+++ include/parser.h
@@ -1,11 +1,35 @@
-#pragma once
-
 /**@<parser.h>::**/
 
+/*
+ * Bacharelado em Ciências da Computação
+ * UNESP Rio Claro
+ * Período Integral
+ * 
+ */
+
+/* 
+ * Desenvolvido pelo grupo 3:
+ * Bionda Rozin
+ * Matheus Missio Francisco
+ * Nicholas Seiti Handa
+ * Nikolas Gomes de Sá
+ * 
+ */
+  
+ /*
+  * Data de criação: 21/02/2021
+  * 
+  */
+
+#pragma once
+/* "Prevenção" contra includes e declarações repetidas" */
+
+/* Includes */
 #include <stdio.h>
 #include <stdlib.h>
 #include <tokens.h>
 
+/* Definição do cabeçalho de funções */
 void declarative(void);
 void imperative(void);
 void vardecl(void);
@@ -23,10 +47,10 @@ int expr(int);
 int smpexpr(int);
 int term(int);
 int fact(int);
-
-
 void match(int expected);
 int gettoken(FILE *);
+
+/* Definição de variáveis externas */
 extern int lookahead;
 extern char lexeme[];
 extern FILE *source;
