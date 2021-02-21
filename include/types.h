97275eb934e8e7d65e54019f3bc9372c66264b45
Bionda Rozin <51840763+BiondaR@users.noreply.github.com>
2021-02-21 16:31:33 -0300

    Update types.h

--- include/types.h
+++ include/types.h
@@ -1,3 +1,27 @@
+/**@<types.h>::**/
+
+/*
+ * Bacharelado em Ciências da Computação
+ * UNESP Rio Claro
+ * Período Integral
+ * 
+ */
+
+ /* 
+ * Desenvolvido pelo grupo 3:
+ * Bionda Rozin
+ * Matheus Missio Francisco
+ * Nicholas Seiti Handa
+ * Nikolas Gomes de Sá
+ * 
+ */
+  
+ /*
+  * Data de criação: 21/02/2021
+  *
+  */
+
+/* Definição de constantes de tipos internos */
 enum {
 	BOOL = 1,
 	INT32,
