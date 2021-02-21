2ed5f492b859d7bca16691e83e3cc9f32c33d326
Bionda Rozin <51840763+BiondaR@users.noreply.github.com>
2021-02-21 16:28:45 -0300

    Update keywords.h

--- include/keywords.h
+++ include/keywords.h
@@ -1,7 +1,33 @@
-#pragma once
+/**@<keywords.h>::**/
 
+/*
+ * Bacharelado em Ciências da Computação
+ * UNESP Rio Claro
+ * Período Integral
+ * 
+ */
+
+ /* 
+ * Desenvolvido pelo grupo 3:
+ * Bionda Rozin
+ * Matheus Missio Francisco
+ * Nicholas Seiti Handa
+ * Nikolas Gomes de Sá
+ * 
+ */
+  
+ /*
+  * Data de criação: 21/02/2021
+  *
+  */
+
+#pragma once
+/* "Prevenção" contra includes e declarações repetidas" */
+
+/* Definição do cabeçalho da função iskeywors */
 int iskeyword(const char *);
 
+/* Definição das constantes correspondentes às palavras chave */
 enum {
 	BEGIN =  10001,
 	NOT,
