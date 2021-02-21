2712c9f5b69868af76f544c01267d28b77379181
Bionda Rozin <51840763+BiondaR@users.noreply.github.com>
2021-02-21 16:30:07 -0300

    Update constants.h

--- include/constants.h
+++ include/constants.h
@@ -1,3 +1,26 @@
+/**@<constants.h>::**/
+
+/*
+ * Bacharelado em Ciências da Computação
+ * UNESP Rio Claro
+ * Período Integral
+ * 
+ */
+
+/* 
+ * Desenvolvido pelo grupo 3:
+ * Bionda Rozin
+ * Matheus Missio Francisco
+ * Nicholas Seiti Handa
+ * Nikolas Gomes de Sá
+ * 
+ */
+  
+ /*
+  * Data de criação: 21/02/2021
+  *
+  */
+
+/* Definição de constantes */
 #define MAXIDLEN 32
 #define MAXSTBSIZE 0x10000
-
