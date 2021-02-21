7c897dbac6f77f86b7a72fd3f05a1921d76de1f4
Bionda Rozin <51840763+BiondaR@users.noreply.github.com>
2021-02-21 16:38:03 -0300

    Update mypas.h

--- include/mypas.h
+++ include/mypas.h
@@ -1,9 +1,34 @@
-#pragma once
-
 /**@<mypas.h>::**/
 
+/*
+ * Bacharelado em Ciências da Computação
+ * UNESP Rio Claro
+ * Período Integral
+ * 
+ */
+
+/* 
+ * Desenvolvido pelo grupo 3:
+ * Bionda Rozin
+ * Matheus Missio Francisco
+ * Nicholas Seiti Handa
+ * Nikolas Gomes de Sá
+ * 
+ */
+  
+ /*
+  * Data de criação: 21/02/2021
+  *
+  */
+
+#pragma once
+/* "Prevenção" contra includes e declarações repetidas" */
+
+/* Includes */
 #include <stdio.h>
 #include <stdlib.h>
 #include <tokens.h>
+
+/* Definição do cabeçalho de funções */
 int gettoken(FILE *);
 void mypas(void);
