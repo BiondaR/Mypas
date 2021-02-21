4f0c0d4d7ef4e30afa971d671ac39715397be417
Bionda Rozin <51840763+BiondaR@users.noreply.github.com>
2021-02-21 16:36:44 -0300

    Update pseudocode.h

--- include/pseudocode.h
+++ include/pseudocode.h
@@ -1,6 +1,30 @@
 /**@<pseudocode.h>::**/
 
+/*
+ * Bacharelado em Ciências da Computação
+ * UNESP Rio Claro
+ * Período Integral
+ * 
+ */
+
+/* 
+ * Desenvolvido pelo grupo 3:
+ * Bionda Rozin
+ * Matheus Missio Francisco
+ * Nicholas Seiti Handa
+ * Nikolas Gomes de Sá
+ * 
+ */
+  
+ /*
+  * Data de criação: 21/02/2021
+  * 
+  */
+
 #pragma once
+/* "Prevenção" contra includes e declarações repetidas" */
+
+/* Definição do cabeçalho de funções */
 void not(void);
 void negate(int);
 void move(int type, const char *src, const char *dest);
