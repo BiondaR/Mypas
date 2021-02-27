b2d84b02c495bf5af640d98e2e69d53effa28bca
Bionda Rozin <51840763+BiondaR@users.noreply.github.com>
2021-02-27 08:29:57 -0300

    Update symtab.c

--- symtab.c
+++ symtab.c
@@ -21,17 +21,18 @@
  *
  */
 
-#include <string.h>
-#include <stdio.h>
-#include <constants.h>
 #include <symtab.h>
 
 /*********************************
  * symtab.h::
-typedef
-struct __symtab__ {
-	char symbol[MAXIDLEN+1];
-	int type;
+typedef struct __symtab__ {
+    char symbol[MAXIDLEN+1];
+    char offset[MAXIDLEN+1];
+    int type;
+    int data_size;
+    int lexical_level;
+    int objtype;
+    int transp_type;
 }
 SYMTAB;
 *********************************/
@@ -40,22 +41,54 @@ SYMTAB symtab[MAXSTBSIZE];
 int symtab_next_entry = 0;
 int symtab_lookup(const char *symbol)
 {
-	int i;
-	for (i = symtab_next_entry - 1; i > -1; i--) {
-		if (strcmp(symtab[i].symbol, symbol) == 0) return i;
-	}
-	return i;
+    for (symtab_entry = symtab_next_entry - 1; symtab_entry > -1; symtab_entry--) {
+        if (strcmp(symtab[symtab_entry].symbol, symbol) == 0) return symtab_entry;
+    }
+    return symtab_entry = -1;
 }
-/**@symtab_rtrvtype::
- * returns the type assigned to the symbol passed as argument,
- * otherwise, if the symbol was not found, it returns a negative integer
- */
-int symtab_rtrvtype(const char *symbol)
-{
-	int i = symtab_lookup(symbol);
-	if (i < 0) {
-		fprintf(stderr, "symtab_rtrvtype: %s undeclared\n", symbol);
-		return i;
-	}
-	return symtab[i].type;
+
+int symtab_entry;
+// int symtab_rtrvtype(const char *symbol, int lexical_level)
+// {
+//     symtab_entry = symtab_lookup(symbol, lexical_level);
+//     if (symtab_entry < 0) {
+//         fprintf(stderr, "symtab_rtrvtype: %s undeclared\n", symbol);
+//         return symtab_entry;
+//     }
+//     return symtab[symtab_entry].type;
+// }
+
+int symtab_append(const char *symbol, int lexical_level, int objtype, int transp_type) {
+    if ( symtab_lookup(symbol) < 0 || symtab[symtab_entry].lexical_level <= lexical_level) {
+        strcpy(symtab[symtab_next_entry].symbol, symbol);
+        sprintf(symtab[symtab_next_entry].offset, "%s[%d]", symbol, lexical_level);
+        symtab[symtab_next_entry].lexical_level = lexical_level;
+        symtab[symtab_next_entry].objtype = objtype;
+        symtab[symtab_next_entry].transp_type = transp_type;
+        return symtab_next_entry++;
+    } else {
+	    /* essa parte pode ser modularizada (aula  17/2 min 11) */
+        fprintf(stderr, "symtab_append: %s multiply defined in current lexical level %d\n", symbol, lexical_level);
+        semantic_error++;
+        return -2;
+    }
+}
+
+void symtab_update_type(int start, int type) {
+    int i;
+    for (i = start; i < symtab_next_entry; i++) {
+        symtab[i].type = type;
+        switch(type) {
+            case BOOL:
+                symtab[i].data_size = 1; break;
+            case INT32:
+            case FLT32:
+                symtab[i].data_size = 4; break;
+            case INT64:
+            case FLT64:
+                symtab[i].data_size = 8; break;
+            default:
+                symtab[i].data_size = 0;
+        }
+    }
 }
