593576e9c3e97b72ab943fa88c646942de810276
Bionda Rozin <51840763+BiondaR@users.noreply.github.com>
2021-03-19 11:08:12 -0300

    Update lexer.h

--- include/lexer.h
+++ include/lexer.h
@@ -1,5 +1,26 @@
 /**@<lexer.h>::**/
 
+/*
+ * Bacharelado em Ciências da Computação
+ * UNESP Rio Claro
+ * Período Integral
+ * 
+ */
+
+/* 
+ * Desenvolvido pelo grupo 3:
+ * Bionda Rozin
+ * Matheus Missio Francisco
+ * Nicholas Seiti Handa
+ * Nikolas Gomes de Sá
+ * 
+ */
+  
+ /*
+  * Data de criação: 21/02/2021
+  *
+  */
+
 #pragma once
 /* "Prevenção" contra includes e declarações repetidas" */
 
